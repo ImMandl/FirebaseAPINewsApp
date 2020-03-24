f4e9ea9714f1792766b7da23d2bb7a554b810a14
Amund Bjornstad <amund.bjornstad@outlook.com>
2020-03-24 19:37:16 +0100

    added modal

--- app/App.svelte
+++ app/App.svelte
@@ -1,7 +1,9 @@
 <script>
+  import { showModal } from "svelte-native";
+  import Article from "./modals/Article.svelte";
   import FirestoreParser from "firestore-parser";
 
-  let items = [];
+  let articles = [];
 
   // API URL
   const baseUrl = "https://firestore.googleapis.com/v1/";
@@ -15,17 +17,27 @@
       .then(response => response.json())
       // sends json trough the firestore parser making it easier to read
       .then(json => FirestoreParser(json))
-      .then(parsed => (items = parsed.documents))
+      .then(parsed => (articles = parsed.documents))
       // picks up errors and sends it to the console log
       .catch(error => console.log(error));
   };
 
   getArticles();
+
+  const showArticle = async article => {
+    await showModal({
+      page: Article,
+      props: {
+        article: article
+      }
+    });
+  };
 </script>
 
 <style>
   .spinner {
     margin-top: 24;
+    color: #800020;
   }
 
   .main {
@@ -55,16 +67,16 @@
   <actionBar title="Bjørneposten" />
   <scrollView class="main">
     <stackLayout>
-      {#each items as item}
-        <flexboxLayout class="article">
+      {#each articles as article}
+        <flexboxLayout class="article" on:tap={() => showArticle(article)}>
           <image
-            src={item.fields.url}
+            src={article.fields.url}
             alt="article image"
             stretch="aspectFit" />
           <stackLayout class="article-tekst">
-            <label class="h2" text={item.fields.tittel} />
-            <label class="body" text={item.fields.tid} />
-            <label class="body" text={item.fields.ingress} />
+            <label class="h2" text={article.fields.tittel} />
+            <label class="body" text={article.fields.tid} />
+            <label class="body" text={article.fields.ingress} />
           </stackLayout>
         </flexboxLayout>
       {:else}
---
+++ app/modals/Article.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { closeModal } from "svelte-native";
+  export let article;
+</script>
+
+<style>
+  .bilde {
+    margin-bottom: 16;
+  }
+  .ingress {
+    font-weight: bold;
+    font-size: 16;
+    margin: 16 0;
+    line-height: 18;
+  }
+  .brodtekst {
+    font-size: 14;
+    line-height: 16;
+  }
+</style>
+
+<frame>
+  <page>
+    <scrollView>
+      <stackLayout style="padding:24">
+        <image
+          src={article.fields.url}
+          class="bilde"
+          alt="article image"
+          stretch="aspectFit" />
+        <label textWrap="true" class="h1" text={article.fields.tittel} />
+        <label textWrap="true" class="ingress" text={article.fields.ingress} />
+        <label textWrap="true" class="brodtekst" text={article.fields.tekst} />
+        <button on:tap={() => closeModal()} text="Tilbake" />
+      </stackLayout>
+    </scrollView>
+  </page>
+</frame>
